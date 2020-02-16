ec891766e904075e5e994938a17c3fd39ad9ff37
邓艳 <denyan7373@163.com>
2020-02-16 16:56:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# ncp_map
+
+## 《Vue实战开发百度实时大数据疫情地图》
+用vue的脚手架搭建框架
+```
+vue create ncp_map
+```
+
+安装依赖
+```
+npm install axios jsonp
+```
+
+疫情数据来源于新浪的接口，是jsonp接口，不是axios。接口地址：
+```
+https://interface.sina.cn/news/wap/fymap2020_data.d.json?_=1580892522427
+```
+
+没有做定时刷新，为啥呢，因为我懒，加个定时器就行了
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="hello">
+    <div ref="mapbox" style="width:700px;height:500px"></div>
+  </div>
+</template>
+
+<script>
+import "echarts/map/js/china.js";
+import echarts from "echarts";
+import jsonp from "jsonp";
+
+const option = {
+  title: {
+    text: "疫情地图"
+  },
+  tooltip: {
+    trigger: "item",
+    textStyle: {
+      fontSize: 12
+    }
+  },
+  series: [
+    {
+      name: "确诊人数",
+      type: "map",
+      mapType: "china",
+      zoom: 1.2,
+      label: {
+        show: true, //显示省份标签
+        fontSize: 10
+      },
+      itemStyle: {
+        emphasis: {
+          areaColor: "#AAAAAA"
+        }
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 12,
+          color: "#fff"
+        }
+      },
+      data: []
+    }
+  ],
+  visualMap: [
+    {
+      type: "piecewise",
+      pieces: [
+        { min: 10000 },
+        { min: 1000, max: 9999 },
+        { min: 100, max: 999 },
+        { min: 10, max: 99 },
+        { min: 1, max: 9 }
+      ],
+      inRange: {
+        color: ["RGB(255,192,177)", "RGB(156,5,5)"]
+      }
+    }
+  ]
+};
+
+export default {
+  name: "HelloWorld",
+  mounted() {
+    this.getData();
+    this.myChart = echarts.init(this.$refs.mapbox);
+    this.myChart.setOption(option);
+  },
+  methods: {
+    getData() {
+      jsonp(
+        "https://interface.sina.cn/news/wap/fymap2020_data.d.json?_=1580892522427",
+        {},
+        (err, data) => {
+          if (!err) {
+            // console.log(data)
+            let list = data.data.list.map(item => ({
+              name: item.name,
+              value: item.value
+            }));
+            option.series[0].data = list;
+            this.myChart.setOption(option);
+          }
+        }
+      );
+    }
+  }
+};
+</script>
